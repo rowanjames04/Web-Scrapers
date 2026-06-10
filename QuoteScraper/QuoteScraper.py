fa248b68c490eb177cdd2fb28f13fb1048687345
rowanjames04 <r04james@outlook.com>
2026-06-10 18:08:58 +1000

    feat: Added csv output support for QuoteScraper

--- QuoteScraper/QuoteScraper.py
+++ QuoteScraper/QuoteScraper.py
@@ -1,15 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 page_to_scrape = requests.get("http://quotes.toscrape.com")
 soup = BeautifulSoup(page_to_scrape.text, "html.parser")
 
-#quotes
+#quotes and authors being scraped
 quotes = soup.findAll("span", attrs = {"class": "text"})
-
-#authors
 authors = soup.findAll("small", attrs = {"class": "author"})
 
+file = open("scraped_quotes.csv", "w", newline='')
+writer = csv.writer(file)
+
+writer.writerow(["QUOTES", "AUTHORS"])
 
 for quote, author in zip(quotes, authors):
     print(quote.text + " - " + author.text)
+    writer.writerow([quote.text, author.text])
+
+file.close()
